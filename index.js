fe6427e5dd1cade6f573d591984378dde3f8154f
Allfoy <allfi09@hotmail.com>
2024-12-23 15:29:19 +0100

    removed depricated command

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(require('cors')());
 
 // MongoDB Setup
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected!'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB!'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Message Schema
 const messageSchema = new mongoose.Schema({
