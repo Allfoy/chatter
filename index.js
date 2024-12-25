4ed19abff0b2dd828dc5ddbde5d09c8dd22b2e36
Allfoy <allfi09@hotmail.com>
2024-12-25 19:21:26 +0100

    just formatted

--- index.js
+++ index.js
@@ -1,60 +1,65 @@
-require('dotenv').config();
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const mongoose = require("mongoose");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: 'https://allfoy.github.io',
-    methods: ['GET', 'POST']
-  }
+	cors: {
+		origin: "https://allfoy.github.io",
+		methods: ["GET", "POST"],
+	},
 });
 
 // Middleware
-const cors = require('cors');
+const cors = require("cors");
 
 // Allow your GitHub Pages origin
-app.use(cors({ origin: 'https://allfoy.github.io' }));
+app.use(cors({ origin: "https://allfoy.github.io" }));
 
 // Other middleware (like express.json) below this
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Setup
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected to MongoDB!'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => console.log("Connected to MongoDB!"))
+	.catch((err) => console.error("MongoDB connection error:", err));
 
 // Message Schema
 const messageSchema = new mongoose.Schema({
-  username: String,
-  message: String,
-  timestamp: { type: Date, default: Date.now }
+	username: String,
+	message: String,
+	timestamp: { type: Date, default: Date.now },
 });
-const Message = mongoose.model('Message', messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 // Socket.io Logic
-io.on('connection', (socket) => {
-  console.log('A user connected.');
+io.on("connection", (socket) => {
+	console.log("A user connected.");
 
-  // Emit previous messages
-  Message.find().then(messages => socket.emit('previousMessages', messages));
+	// Emit previous messages
+	Message.find().then((messages) =>
+		socket.emit("previousMessages", messages)
+	);
 
-  // Save & broadcast messages
-  socket.on('chatMessage', async (data) => {
-    const newMessage = new Message(data);
-    await newMessage.save();
-    io.emit('chatMessage', newMessage);
-  });
+	// Save & broadcast messages
+	socket.on("chatMessage", async (data) => {
+		const newMessage = new Message(data);
+		await newMessage.save();
+		io.emit("chatMessage", newMessage);
+	});
 
-  socket.on('disconnect', () => console.log('A user disconnected.'));
+	socket.on("disconnect", () => console.log("A user disconnected."));
 });
 
 // Routes
-app.get('/', (req, res) => res.send('Chat App is running.'));
+app.get("/", (req, res) => res.send("Chat App is running."));
 
 // Start the server
-server.listen(3000, () => console.log('Server is running on http://localhost:3000'));
+server.listen(3000, () =>
+	console.log("Server is running on http://localhost:3000")
+);
